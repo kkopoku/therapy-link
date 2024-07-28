a7c68b51344d83815f7af311287c6c9591672ec4
kkopoku <kk.opoku@outlook.com>
2024-07-28 13:39:37 +0000

    created client schema

---
+++ backend/src/models/client.model.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+import User from "./user.model"
+
+
+const clientSchema = new Schema(
+  {
+    momoNumber:{
+      type: String,
+      required: true,
+    },
+    momoNetwork:{
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Client = User.discriminator("Client", clientSchema);
+
+export default Client;
