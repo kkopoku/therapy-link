c1f3b9c0bc27a30aac1fcfd4ec591490082f27c8
kkopoku <kk.opoku@outlook.com>
2024-07-29 08:12:03 +0000

    index setup wip

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,34 @@
 import express, { Request, Response } from 'express'
 import { connectDB } from "./database/connection"
+import userRouter from './routes/user.route'
 
 
-const app = express();
-const port = process.env.PORT || 3001;
+const app = express()
+const port = process.env.PORT || 3001
+const prefix = "/api/v1"
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 
-const startServer = async () => {
+app.use(`${prefix}/user`, userRouter);
+
+const run = async () => {
   try {
     await connectDB()
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`)
     })
   } catch (error) {
-    console.error('Failed to connect to the database', error);
+    console.error('Failed to connect to the database', error)
   }
 }
 
 
-startServer()
+run()
