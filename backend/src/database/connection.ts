01053cf524e10f36d6f73ed08c538470367df803
kkopoku <kk.opoku@outlook.com>
2024-08-01 19:58:50 +0000

    added log and dotconfig import

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import "dotenv/config";
+
 
 export const connectDB = async () => {
     try {
         if (process.env.MONGO_URI) {
             await mongoose.connect(process.env.MONGO_URI);
+            console.log("Database connection established ...")
         }
     } catch (error) {
         console.error(error);
