e8440cc4ea7c7e658d9fafe20be6474ef239f093
kkopoku <kk.opoku@outlook.com>
2024-08-08 12:11:12 +0000

    installed jwt

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,38 +1,60 @@
-import mongoose, { Error } from "mongoose"
-import bcrypt from "bcrypt"
+import mongoose, { Document, Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  primaryPhone: string;
+  userType: 'Administrator' | 'Client' | 'Therapist';
+  secondaryPhone?: string;
+  check(): string; // Custom method
+  createJWT(): string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
 
-const Schema = mongoose.Schema
-const userSchema = new Schema(
+const userSchema = new Schema<IUser>(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     primaryPhone: { type: String, required: true },
-    secondaryPhone: { type: String, required: false },
+    secondaryPhone: { type: String },
     userType: { type: String, required: true, enum: ['Therapist', 'Client', 'Administrator'] },
   },
   { discriminatorKey: 'userType', timestamps: true }
-)
+);
 
-userSchema.pre("save", async function (next) {
-  try{
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
-  }catch(e:any){
-    next(e)
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (e: any) {
+    next(e);
   }
-})
+});
 
-// userSchema.methods.createJWT = function () {
-//   return jwt.sign({ employee_id: this._id, first_name: this.first_name, last_name: this.last_name, organisation_id: this.organisation_id },
-//       process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME })
-// }
+userSchema.methods.createJWT = function (): string {
+  return jwt.sign(
+    {
+      employee_id: this._id,
+      first_name: this.first_name,
+      last_name: this.last_name,
+      organisation_id: this.organisation_id,
+    },
+    process.env.JWT_SECRET!,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
 
-userSchema.methods.comparePassword = async function (candidatePassword:string) {
-  const isMatch = await bcrypt.compare(candidatePassword, this.password)
-  return isMatch
-}
+userSchema.methods.check = function (): string {
+  return this.userType;
+};
 
-const User = mongoose.model('User', userSchema)
-export default User
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
+const User = mongoose.model<IUser>('User', userSchema);
+export default User;
