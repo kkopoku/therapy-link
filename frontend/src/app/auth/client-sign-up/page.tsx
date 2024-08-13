039c431c5475b689d57378885eace12cf2d3a634
kkopoku <kk.opoku@outlook.com>
2024-08-13 22:14:36 +0000

    make client-sign-up use Unauthenticated layout

--- frontend/src/app/auth/client-sign-up/page.tsx
+++ frontend/src/app/auth/client-sign-up/page.tsx
@@ -2,8 +2,7 @@
 
 import QuestionCard from "@/components/cards/QuestionCard";
 import React, { useState } from "react";
-import TopNavigation from "@/components/navigation/TopNavigation";
-import BottomNavigation from "@/components/navigation/BottomNavigation";
+import UnauthenticatedLayout from "@/components/layouts/UnauthenticatedLayout";
 
 export default function ClientSignUpPage() {
   interface Question {
@@ -530,160 +529,153 @@ export default function ClientSignUpPage() {
       });
 
       if (!response.ok) {
-        let errorBody = await response.json()
-        console.log(errorBody)
-        throw new Error(`Request failed with status ${response.status}`)
+        let errorBody = await response.json();
+        console.log(errorBody);
+        throw new Error(`Request failed with status ${response.status}`);
       }
 
-      response = await response.json()
-      
-
+      response = await response.json();
     } catch (e: any) {
       console.log(e.message);
     }
   }
 
   return (
-    <div>
-      <main className="flex min-h-screen bg-slate-100 flex-col items-center font-sans text-black">
-        <TopNavigation />
-
-        <div className="flex flex-col items-center lg:max-w-4xl justify-center text-center pt-10 lg:pt-15">
-          <div className="text-3xl font-extralight">
-            Help us match you to the right Therapist
-          </div>
-          <div className="font-extralight">
-            It's important to have a therapist who you can establish a personal
-            connection with. The following questions are designed to help match
-            you to a licensed therapist based on your needs and personal
-            preferences.
-          </div>
+    <UnauthenticatedLayout>
+      <div className="flex flex-col items-center lg:max-w-4xl justify-center text-center pt-10 lg:pt-15">
+        <div className="text-3xl font-extralight">
+          Help us match you to the right Therapist
         </div>
+        <div className="font-extralight">
+          It's important to have a therapist who you can establish a personal
+          connection with. The following questions are designed to help match
+          you to a licensed therapist based on your needs and personal
+          preferences.
+        </div>
+      </div>
 
-        {showSignUp && (
-          <div className="flex items-center justify-center py-10 max-w-[80vh]">
-            <QuestionCard
-              question={question.question}
-              type={question.type}
-              options={question.options}
-              next={nextQuestion}
-              back={previousQuestion}
-            />
-          </div>
-        )}
+      {!showSignUp && (
+        <div className="flex items-center justify-center py-10 max-w-[80vh]">
+          <QuestionCard
+            question={question.question}
+            type={question.type}
+            options={question.options}
+            next={nextQuestion}
+            back={previousQuestion}
+          />
+        </div>
+      )}
 
-        {!showSignUp && (
-          <div className="flex items-center justify-center py-10 w-full">
-            <section className="w-full">
-              <div className="flex flex-col items-center justify-center px-6 mx-auto lg:py-0 w-full">
-                <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-[#1A3634]">
-                  <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-2xl font-medium leading-tight tracking-tight text-white md:text-2xl">
-                      Create an account
-                    </h1>
+      {showSignUp && (
+        <div className="flex items-center justify-center py-10 w-full">
+          <section className="w-full">
+            <div className="flex flex-col items-center justify-center px-6 mx-auto lg:py-0 w-full">
+              <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-[#1A3634]">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 className="text-2xl font-medium leading-tight tracking-tight text-white md:text-2xl">
+                    Create an account
+                  </h1>
 
-                    <form className="space-y-4 md:space-y-6" action={signUp}>
-                      <div>
-                        <label
-                          htmlFor="email"
-                          className="block mb-2 text-sm font-light text-white"
-                        >
-                          Your email
-                        </label>
-                        <input
-                          type="email"
-                          name="email"
-                          id="email"
-                          className=" text-gray-900 text-sm rounded-lg w-full p-2.5"
-                          placeholder="name@company.com"
-                          onChange={(e) => setEmail(e.target.value)}
-                          value={email}
-                          required
-                        />
-                      </div>
-                      <div>
-                        <label
-                          htmlFor="password"
-                          className="block mb-2 text-sm font-light text-white"
-                        >
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          name="password"
-                          id="password"
-                          className={`bg-gray-50 text-sm rounded-lg w-full p-2`}
-                          placeholder="••••••••"
-                          onChange={(e) => handlePasswordChange(e, "password")}
-                          value={password}
-                          required
-                        />
-                      </div>
-                      <div>
-                        <label
-                          htmlFor="confirm-password"
-                          className="block mb-2 text-sm font-light text-white"
-                        >
-                          Confirm password
-                        </label>
-                        <input
-                          type="password"
-                          name="confirm-password"
-                          id="confirm-password"
-                          className={`border bg-gray-50 ${
-                            !passwordsMatch
-                              ? "bg-red-50 border-red-700"
-                              : "bg-green-50 border-green-700 outline-green-700"
-                          } font-light text-sm rounded-lg block w-full p-2`}
-                          placeholder="••••••••"
-                          onChange={(e) => handlePasswordChange(e)}
-                          value={confirmPassword}
-                          required
-                        />
-                      </div>
-                      <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                          <input
-                            id="terms"
-                            aria-describedby="terms"
-                            type="checkbox"
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
-                            required
-                          />
-                        </div>
-                        <div className="ml-3 text-sm">
-                          <label
-                            htmlFor="terms"
-                            className="font-light text-gray-500 dark:text-gray-300"
-                          >
-                            I accept the{" "}
-                            <a className="font-medium` hover:underline dark:text-primary-500">
-                              Terms and Conditions
-                            </a>
-                          </label>
-                        </div>
-                      </div>
-                      <button
-                        type="submit"
-                        className="w-full text-white bg-black font-light rounded-lg text-sm px-5 py-2.5 text-center"
+                  <form className="space-y-4 md:space-y-6" action={signUp}>
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-light text-white"
                       >
-                        Create an account
-                      </button>
-                      <p className="text-sm font-light text-white">
-                        Already have an account? &nbsp;
-                        <a className="font-medium text-gray-400 hover:underline dark:text-primary-500">
-                          Login here
-                        </a>
-                      </p>
-                    </form>
-                  </div>
+                        Your email
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className=" text-gray-900 text-sm rounded-lg w-full p-2.5"
+                        placeholder="name@company.com"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-light text-white"
+                      >
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        className={`bg-gray-50 text-sm rounded-lg w-full p-2`}
+                        placeholder="••••••••"
+                        onChange={(e) => handlePasswordChange(e, "password")}
+                        value={password}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="confirm-password"
+                        className="block mb-2 text-sm font-light text-white"
+                      >
+                        Confirm password
+                      </label>
+                      <input
+                        type="password"
+                        name="confirm-password"
+                        id="confirm-password"
+                        className={`border bg-gray-50 ${
+                          !passwordsMatch
+                            ? "bg-red-50 border-red-700"
+                            : "bg-green-50 border-green-700 outline-green-700"
+                        } font-light text-sm rounded-lg block w-full p-2`}
+                        placeholder="••••••••"
+                        onChange={(e) => handlePasswordChange(e)}
+                        value={confirmPassword}
+                        required
+                      />
+                    </div>
+                    <div className="flex items-start">
+                      <div className="flex items-center h-5">
+                        <input
+                          id="terms"
+                          aria-describedby="terms"
+                          type="checkbox"
+                          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                          required
+                        />
+                      </div>
+                      <div className="ml-3 text-sm">
+                        <label
+                          htmlFor="terms"
+                          className="font-light text-gray-500 dark:text-gray-300"
+                        >
+                          I accept the{" "}
+                          <a className="font-medium` hover:underline dark:text-primary-500">
+                            Terms and Conditions
+                          </a>
+                        </label>
+                      </div>
+                    </div>
+                    <button
+                      type="submit"
+                      className="w-full text-white bg-black font-light rounded-lg text-sm px-5 py-2.5 text-center"
+                    >
+                      Create an account
+                    </button>
+                    <p className="text-sm font-light text-white">
+                      Already have an account? &nbsp;
+                      <a className="font-medium text-gray-400 hover:underline dark:text-primary-500">
+                        Login here
+                      </a>
+                    </p>
+                  </form>
                 </div>
               </div>
-            </section>
-          </div>
-        )}
-      </main>
-      <BottomNavigation />
-    </div>
+            </div>
+          </section>
+        </div>
+      )}
+    </UnauthenticatedLayout>
   );
 }
