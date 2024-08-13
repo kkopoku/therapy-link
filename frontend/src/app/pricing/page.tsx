f6a465d4fbeb25e670fdb0d460a19f23ed8350f6
kkopoku <kk.opoku@outlook.com>
2024-08-13 22:14:51 +0000

    pricing page wip

---
+++ frontend/src/app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import UnauthenticatedLayout from "@/components/layouts/UnauthenticatedLayout"
+import TopNavigation from "@/components/navigation/TopNavigation"
+import BottomNavigation from "@/components/navigation/BottomNavigation"
+
+
+export default function PricingPage(){
+    return(
+        <UnauthenticatedLayout>
+            hi bub
+        </UnauthenticatedLayout>
+    )
+}
