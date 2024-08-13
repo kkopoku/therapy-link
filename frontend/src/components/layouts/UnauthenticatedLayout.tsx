06906c8bc8411adab08c6e04c3ffdc5d2223bcbb
kkopoku <kk.opoku@outlook.com>
2024-08-13 22:15:14 +0000

    created unauthenticated layout

---
+++ frontend/src/components/layouts/UnauthenticatedLayout.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from "react"
+import TopNavigation from "../navigation/TopNavigation"
+import BottomNavigation from "../navigation/BottomNavigation"
+
+interface LayoutProps {
+    children?: React.ReactNode
+}
+
+const UnauthenticatedLayout: React.FC<LayoutProps> = ({children}) => {
+    return(
+        <div>
+            <main className="flex min-h-screen bg-slate-50 flex-col items-center font-sans text-black">
+                <TopNavigation />
+                {children}
+            </main>
+            <BottomNavigation />
+        </div>
+    )
+}
+
+export default UnauthenticatedLayout
+
