5600cf37de2511283c63e68efeb002c16f9ecd81
kkopoku <kk.opoku@outlook.com>
2024-08-15 05:19:55 +0000

    created middleware.ts

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,15 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(function middleware(req) {
+  if (!req.nextauth.token) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/auth/login";
+    return NextResponse.rewrite(url);
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/dashboard/:path*"],// Routes you want to protect
+};
