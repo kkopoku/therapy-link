572e9b043a8146246f156a4ca99d1e364aedb29c
kkopoku <kk.opoku@outlook.com>
2024-08-15 05:20:22 +0000

    configured nextauth in root lyout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Provider } from "@/providers/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,12 +15,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  session
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
+  session: any
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Provider>
+        <body className={inter.className}>{children}</body>
+      </Provider>
     </html>
   );
 }
