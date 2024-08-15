b99e876248dd98ae50f8647950355eb3f0b7eb0a
kkopoku <kk.opoku@outlook.com>
2024-08-15 05:18:08 +0000

    created login function

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -3,14 +3,14 @@ import { Request, Response } from "express"
 import Joi from "joi"
 import { createUser } from "../library/user.library"
 
-export async function registerUser(req: Request, res: Response) {
-    
-    enum UserType {
-        Administrator = "Administrator",
-        Client = "Client",
-        Therapist = "Therapist"
-    }
-      
+enum UserType {
+    Administrator = "Administrator",
+    Client = "Client",
+    Therapist = "Therapist"
+}
+
+
+export async function registerUser(req: Request, res: Response) {   
     const schema = Joi.object({
         email: Joi.string().email().required(),
         password: Joi.string().min(8).required(),
@@ -42,6 +42,54 @@ export async function registerUser(req: Request, res: Response) {
       return res
         .status(200)
         .json({ user, message: "You have successfully created your account" })
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export async function login(req: Request, res: Response): Promise<void | {}> {
+    const schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(8).required(),
+        type: Joi.string().valid(...Object.values(UserType)).required(),
+    })
+
+    const { error, value } = schema.validate(req.body)
+
+    if (error){
+        return res.status(400).json({
+            message: error.details[0].message,
+            status: "failed"
+        })
+    }
+
+    const { email, password } = value
+    try{
+        // const foundUser = await User.findOne({ email }).lean().exec()
+        const foundUser = await User.findOne({ email })
+        if(!foundUser){
+            return res.status(400).json({
+                message: "Invalid credentials",
+                status: "failed"
+            })
+        }
+
+
+        const passwordCheck:boolean = await foundUser.comparePassword(password)
+        if(!passwordCheck){
+            return res.status(400).json({
+                message: "Invalid credentials",
+                status: "failed"
+            })
+        }
+
+        const token = foundUser.createJWT()
+
+        return res
+            .status(200)
+            .json({ foundUser, token, message: "You logged in successfully" })
+
     }catch(error){
         console.log(error)
         return res.status(500).json({ message: "Internal Server Error" })
