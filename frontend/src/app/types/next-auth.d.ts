4b305c80c3f0ba7cf397a6d14fae4d7cef7caaf0
kkopoku <kk.opoku@outlook.com>
2024-08-16 21:08:48 +0000

    added name

--- frontend/src/app/types/next-auth.d.ts
+++ frontend/src/app/types/next-auth.d.ts
@@ -1,29 +1,32 @@
-import NextAuth, { User, type DefaultSession } from "next-auth";
+import NextAuth, { User, type DefaultSession } from "next-auth"
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string;
-      email: string;
-      token: string;
-      type: string;
-    };
+      id: string
+      email: string
+      token: string
+      type: string
+      name: string
+    }
   }
   interface User {
-    id: string;
-      email: string;
-      token: string;
-      type: string;
+    id: string
+    email: string
+    token: string
+    type: string
+    name: string
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     user: {
-      id: string;
-      email: string;
-      token: string;
-      type: string;
-    };
+      id: string
+      email: string
+      token: string
+      type: string
+      name: string
+    }
   }
 }
