e5802611d174dd63ce7e7d1b63871daa1cb8e50c
kkopoku <kk.opoku@outlook.com>
2024-08-31 14:05:22 +0000

    added getUsers route

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser, login } from "../controllers/user.controller";
+import { registerUser, login, getUsers } from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.post("/create", registerUser);
 userRouter.post("/login", login)
+userRouter.get("/:userType", getUsers)
 
 export default userRouter;
