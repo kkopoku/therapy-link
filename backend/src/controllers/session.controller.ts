05f476f4e0e2d4ecd824de25f5d7572d018fe558
kkopoku <kk.opoku@outlook.com>
2024-08-31 14:01:47 +0000

    created session controller

---
+++ backend/src/controllers/session.controller.ts
@@ -0,0 +1,63 @@
+import Session from "../models/session.model"
+import Joi from "joi"
+import { Request, Response } from "express"
+import { sendResponse } from "../library/utils.library"
+import Client from "../models/client.model"
+import Therapist from "../models/therapist.model"
+import mongoose from "mongoose"
+import { objectIdValidator } from "../library/joi.library"
+import Administrator from "../models/administrator.model"
+
+export async function createSession(req: Request, res: Response) {
+    let logtag = "[session.controller.ts][createSession]"
+
+    // validate request body
+    const schema = Joi.object({
+        startDate: Joi.date().required(),
+        endDate: Joi.date().required(),
+        duration: Joi.number().required(),
+        therapistId: objectIdValidator.required(),
+        clientId: objectIdValidator.required(),
+    })
+
+    const { error, value } = schema.validate(req.body)
+
+    if (error) {
+        return res.status(403).json({
+            message: error.details[0].message,
+            status: "failed"
+        })
+    }
+
+    try {
+        // check user and client
+        if (!mongoose.Types.ObjectId.isValid(value.clientId))
+            return res.status(400).json({ message: "Invalid client ID" });
+
+        if (!mongoose.Types.ObjectId.isValid(value.therapistId))
+            return res.status(400).json({ message: "Invalid therapist ID" });
+
+        let foundClient = await Client.findById(value.clientId).lean().exec()
+        console.log(foundClient)
+        if (!foundClient)
+            throw new Error("Client not found")
+
+        let foundTherapist = await Therapist.findById(value.therapistId).lean().exec()
+        if (!foundTherapist)
+            throw new Error("Therapist not found")
+
+        let createdSession = await Session.create(value)
+        return sendResponse(res, 201, {
+            data: createdSession,
+            status: "success",
+            message: "session successfully created"
+        })
+    } catch (error: any) {
+        console.log(`${logtag} Error: ${error}`)
+        return sendResponse(res, 500, {
+            data: null,
+            status: "error",
+            message: error.message
+        })
+    }
+}
