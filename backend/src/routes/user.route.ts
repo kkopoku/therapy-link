5820b0fe0d5bd6f77e4da397a4d45173c4124adf
kkopoku <kk.opoku@outlook.com>
2024-09-01 22:09:25 +0000

    added get user

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,12 +1,20 @@
 import express from "express";
-import { registerUser, login, getUsers, updateClient, updateTherapist } from "../controllers/user.controller";
+import { 
+    registerUser, 
+    login, 
+    getUsers, 
+    updateClient, 
+    updateTherapist, 
+    getUser 
+} from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.post("/create", registerUser);
 userRouter.post("/login", login)
-userRouter.get("/:userType", getUsers)
+userRouter.get("/getAllUsers/:userType", getUsers)
 userRouter.patch("/updateClient", updateClient)
 userRouter.patch("/updateTherapist", updateTherapist)
+userRouter.get("/:id", getUser)
 
 export default userRouter;
