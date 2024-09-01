8c53918cc1b93b0e4e1a59f74ecb825b507afaea
kkopoku <kk.opoku@outlook.com>
2024-09-01 22:06:48 +0000

    making response null if not there

--- backend/src/library/utils.library.ts
+++ backend/src/library/utils.library.ts
@@ -18,13 +18,15 @@ export async function loadModels() {
   return models;
 }
 
-export async function sendResponse<T>(res: Response, statusCode: number = 200, response: ApiResponse<T>){
+export async function sendResponse<T>(res: Response, response: ApiResponse<T>, statusCode: number = 200){
   if (typeof response.status !== "string" || typeof response.message !== "string") {
     throw new Error("Response does not match the ApiResponse contract");
   }
   if (response.data && typeof(response.data) === "object" && "password" in response.data){
       delete response.data.password
   }
+
+  if (!response.data) response.data = null
   return res.status(statusCode).json(response);
 }
 
