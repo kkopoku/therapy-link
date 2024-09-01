ca2963030902e2548d38297f9d4e8b002e176a67
kkopoku <kk.opoku@outlook.com>
2024-09-01 06:16:06 +0000

    added updateClient

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerUser, login, getUsers } from "../controllers/user.controller";
+import { registerUser, login, getUsers, updateClient } from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.post("/create", registerUser);
 userRouter.post("/login", login)
 userRouter.get("/:userType", getUsers)
+userRouter.patch("/updateClient", updateClient)
 
 export default userRouter;
