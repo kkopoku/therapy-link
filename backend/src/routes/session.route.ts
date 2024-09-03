73976c1360f4ae8ffce0768c29bf232272c71052
kkopoku <kk.opoku@outlook.com>
2024-09-03 10:12:27 +0000

    added routes

--- backend/src/routes/session.route.ts
+++ backend/src/routes/session.route.ts
@@ -1,9 +1,12 @@
 import express from "express"
 
-import { createSession } from "../controllers/session.controller"
+import { createSession, getSessions, getSessionDetails } from "../controllers/session.controller"
 
 const sessionRouter = express.Router();
 
 sessionRouter.post("/create", createSession)
+sessionRouter.get("/", getSessions)
+sessionRouter.get("/:id", getSessionDetails)
+
 
 export default sessionRouter;
