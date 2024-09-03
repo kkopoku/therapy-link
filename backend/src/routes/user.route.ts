f6e2edfcdd7eb6657032a88fee4b28da1de7a25e
kkopoku <kk.opoku@outlook.com>
2024-09-03 19:49:21 +0000

    removed unused code

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,20 +1,21 @@
 import express from "express";
 import { 
-    registerUser, 
-    login, 
     getUsers, 
     updateClient, 
     updateTherapist, 
-    getUser 
+    getUser, 
+    getClients
 } from "../controllers/user.controller";
+import { adminAuth } from "../middlewares/auth.middleware";
 
 const userRouter = express.Router();
 
-userRouter.post("/create", registerUser);
-userRouter.post("/login", login)
-userRouter.get("/getAllUsers/:userType", getUsers)
+userRouter.get("/getAllUsers", adminAuth, getUsers)
+
+
 userRouter.patch("/updateClient", updateClient)
 userRouter.patch("/updateTherapist", updateTherapist)
 userRouter.get("/:id", getUser)
+userRouter.get("/clients", getClients)
 
 export default userRouter;
