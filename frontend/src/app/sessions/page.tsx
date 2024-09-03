b2b3656c50168069603eccce30ea3d2b41e3c777
kkopoku <kk.opoku@outlook.com>
2024-09-03 10:14:22 +0000

    sessions page wip

--- frontend/src/app/sessions/page.tsx
+++ frontend/src/app/sessions/page.tsx
@@ -1,18 +1,67 @@
 "use client"
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthenticatedLayout from "@/components/layouts/AuthenticatedLayout";
-
+import { Table } from "flowbite-react"
+import { data } from "../dashboard/page.functions"   //remove this in future
+import { getSessions, createSession } from "./page.functions"
+import { testToast } from "./page.functions"
+import { Toaster } from "react-hot-toast";
+import { useSession } from "next-auth/react";
+import LoadingTable from "@/components/loading/LoadingTable";
 
 export default function SessionsPage(){
 
-    function createSession(){
-        console.log("create session")
-    }
+    const [records, setRecords] = useState<any>([])
+    const [loading, setLoading] = useState<boolean>(true)
+    const { data: session } = useSession()
+    // const user = session?.user
+
+    useEffect(()=>{
+        getSessions(session, setRecords, setLoading)
+    },[session])
 
     return(
         <AuthenticatedLayout pageName="Sessions" navFunctionName="Create Session" navFunction={createSession}>
-            hello blud, you are at sessions
+            <Toaster />
+            <div className="flex flex-grow flex-col gap-y-5 w-full overflow-scroll">
+
+            {!loading && <div className="flex flex-col border-none flex-grow gap-y-5 overflow-hidden">
+                    <label>{("Upcoming Therapy Sessions").toUpperCase()}</label>
+                    <div className="max-h-fit w-full rounded-xl border border-primaryGreen border-dashed relative overflow-y-scroll shadow-md">
+                        <Table hoverable>
+                            <Table.Head className="text-white font-extralight">
+                                <Table.HeadCell className="bg-primaryGreen py-2 font-normal sticky top-0">Start Date</Table.HeadCell>
+                                <Table.HeadCell className="bg-primaryGreen py-2 font-normal sticky top-0">Start Time</Table.HeadCell>
+                                <Table.HeadCell className="bg-primaryGreen py-2 font-normal sticky top-0">End Date</Table.HeadCell>
+                                <Table.HeadCell className="bg-primaryGreen py-2 font-normal sticky top-0">End Time</Table.HeadCell>
+                                <Table.HeadCell className="bg-primaryGreen py-2 font-normal sticky top-0">Therapist Name</Table.HeadCell>
+                                <Table.HeadCell className="bg-primaryGreen py-2 font-normal sticky top-0">Session Type</Table.HeadCell>
+                                <Table.HeadCell className="bg-primaryGreen py-2 font-normal sticky top-0">Duration</Table.HeadCell>
+                                <Table.HeadCell className="bg-primaryGreen py-2 font-normal sticky top-0">Status</Table.HeadCell>
+                            </Table.Head>
+                            <Table.Body className="divide-y">
+                                {records.map((data:any,idx:number)=>
+                                    <Table.Row key={idx} onClick={testToast} className="bg-white hover:cursor-pointer dark:border-gray-700 dark:bg-gray-800 leading-none">
+                                        <Table.Cell className="">{(new Date(data.startDate)).toLocaleDateString()}</Table.Cell>
+                                        <Table.Cell className="">{(new Date(data.startDate)).toLocaleTimeString("en-US",{ hour12: false})}</Table.Cell>
+                                        <Table.Cell className="">{(new Date(data.endDate)).toLocaleDateString()}</Table.Cell>
+                                        <Table.Cell className="">{(new Date(data.endDate)).toLocaleTimeString("en-US",{ hour12: false})}</Table.Cell>
+                                        <Table.Cell className="">{data.endDate}</Table.Cell>
+                                        <Table.Cell className="">{data.clientId}</Table.Cell>
+                                        <Table.Cell className="">{data.clientId}</Table.Cell>
+                                        <Table.Cell className="">{data.status}</Table.Cell>
+                                    </Table.Row>
+                                )}
+                            </Table.Body>
+                        </Table>
+
+                    </div>
+                </div>}
+
+                {loading && <LoadingTable />}
+                
+            </div>
         </AuthenticatedLayout>
     )
 }
