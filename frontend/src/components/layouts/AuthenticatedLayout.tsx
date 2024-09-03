2a1ae94932185f703cd6f7e5dac392d15862e3e8
kkopoku <kk.opoku@outlook.com>
2024-09-03 10:15:11 +0000

    updated authenticated layout style

--- frontend/src/components/layouts/AuthenticatedLayout.tsx
+++ frontend/src/components/layouts/AuthenticatedLayout.tsx
@@ -14,11 +14,11 @@ interface LayoutProps {
 const AuthenticatedLayout: React.FC<LayoutProps> = ({children, pageName, navFunctionName, navFunction}) => {
     return(
         <div>
-            <main className="flex flex-row min-h-screen items-center bg-white w-full font-sans text-black transition-all">
+            <main className="flex flex-row min-h-screen w-screen items-center bg-white font-sans text-black transition-all">
                 <div className="flex items-center w-fit">
                     <SideBar />
                 </div>
-                <div className="flex flex-col flex-grow h-screen pb-3 px-16 w-full items-center">
+                <div className="flex flex-col flex-grow h-screen overflow-x-auto pb-3 px-10 w-full items-center">
                     <div className="flex flex-row justify-between py-5 w-full">
                         <p className="text-lg text-primaryGreen font-semibold">{pageName}</p>
                         <button className="px-2 py-1 hover:bg-secondaryGreen hover:text-white text-sm text-center border-2 
