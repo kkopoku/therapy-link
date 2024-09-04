2569da158c9edb6753b1f0841f05650997a9c81c
kkopoku <kk.opoku@outlook.com>
2024-09-04 20:54:52 +0000

    added getClients

--- frontend/src/app/clients/page.functions.ts
+++ frontend/src/app/clients/page.functions.ts
@@ -1,9 +1,31 @@
+import { Session } from "next-auth";
+
 const endpoint = process.env.NEXT_PUBLIC_API_URL
+
 
 export async function getClients(session:any, setRecords:Function, setLoading:Function){
     if(session){
         const url = `${endpoint}/user/clients`;
-        console.log(session.user.token)
+        await fetch(url, {
+            method: "GET",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${session.user.token}`
+            },
+        })
+        .then(response => response.json())
+        .then((jsonResponse) => {
+            setRecords(jsonResponse.data)
+            setLoading(false)
+        }).catch(error=>console.log(error))
+    }
+}
+
+
+export async function getClientDetails(session:Session, clientId:string, setRecords:Function, setLoading:Function){
+    if(session){
+        const url = `${endpoint}/client/${clientId}`;
         await fetch(url, {
             method: "GET",
             headers: {
