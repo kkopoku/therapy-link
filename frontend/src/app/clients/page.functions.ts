b80fd5769bd16493f3b65ed67d1d549ac9a6866c
kkopoku <kk.opoku@outlook.com>
2024-09-04 17:35:34 +0000

    wrote getClients req function

--- frontend/src/app/clients/page.functions.ts
+++ frontend/src/app/clients/page.functions.ts
@@ -1,7 +1,23 @@
-
+"use server"
 
 const endpoint = process.env.NEXT_PUBLIC_API_URL
 
 export async function getClients(session:any, setRecords:Function, setLoading:Function){
-    console.log("here boy")
+    if(session){
+        const url = `${endpoint}/user/clients`;
+        console.log(session.user.token)
+        await fetch(url, {
+            method: "GET",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${session.user.token}`
+            },
+        })
+        .then(response => response.json())
+        .then((jsonResponse) => {
+            setRecords(jsonResponse.data)
+            setLoading(false)
+        }).catch(error=>console.log(error))
+    }
 }
