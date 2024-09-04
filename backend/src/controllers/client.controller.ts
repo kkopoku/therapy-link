be4ea3d373b7931d34066c58a6068c316bb8445f
kkopoku <kk.opoku@outlook.com>
2024-09-04 23:37:42 +0000

    getClients added

--- backend/src/controllers/client.controller.ts
+++ backend/src/controllers/client.controller.ts
@@ -5,7 +5,6 @@ import { objectId } from "../library/joi.library"
 import { sendResponse } from "../library/utils.library"
 
 export async function getClientDetails(req:Request, res:Response){
-
     const tag = "[client.controller][getClientDetails]"
     const schema = Joi.object({
         id: Joi.string().custom(objectId).required(),
@@ -45,5 +44,35 @@ export async function getClientDetails(req:Request, res:Response){
             status: "error"
         },500)
     }
-    
+}
+
+
+export async function getClients(req:Request, res:Response){
+    const tag = "[client.controller][getClients]"
+    const userType = req.user.type
+    try{
+        let foundClients = []
+        switch(userType){
+            case "Administrator":
+                console.log("came through here")
+                foundClients = await Client.find({})
+                    .populate({path:"therapist", select: "name email primaryPhone"}).lean().exec()
+                break
+            case "Therapist":
+                foundClients = await Client.find({therapist: req.user.id}).lean().exec()
+                break
+            default:
+                console.log(`${tag} Unusual user type detected: ${userType}`)
+                return sendResponse(res,{message:"Unauthorized", status:"failed"}, 401)
+        }
+        sendResponse(res,{message:"Client Details fetched successfully", status: "success", data: foundClients})
+    }catch(error:any){
+        console.log(`${tag} Error: ${error}`)
+        return sendResponse(res,{
+            message:"Internal Server Error",
+            status: "error"
+        },500)
+    }
+
+
 }
