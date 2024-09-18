8a617ab9b783dcbfd3cff7a7b955bd84e2bc4cf6
kkopoku <kk.opoku@outlook.com>
2024-09-18 21:39:38 +0000

    chore: added functionality to start sessions

--- frontend/src/app/sessions/view/[sessionId]/page.tsx
+++ frontend/src/app/sessions/view/[sessionId]/page.tsx
@@ -7,6 +7,8 @@ import { useSession } from "next-auth/react";
 import { getSessionDetails } from "../../page.functions";
 import { Toaster } from "react-hot-toast";
 import PrimaryInput from "@/components/inputs/PrimaryInput";
+import Button from "@/components/buttons/Button";
+import { useRouter } from "next/navigation"
 
 interface SessionDetails {
     _id: string;
@@ -26,6 +28,7 @@ export default function ViewSessionPage() {
     const params = useParams();
     const { data: session } = useSession();
     const sessionId = params.sessionId as string;
+    const router = useRouter()
 
     useEffect(() => {
         setLoading(true);
@@ -36,14 +39,16 @@ export default function ViewSessionPage() {
         <AuthenticatedLayout pageName="View Session">
             <Toaster />
             <div className="w-full flex flex-col flex-grow pt-10">
+
                 <div className="flex h-full flex-row w-full">
                     <div className="grid grid-cols-5 w-full gap-x-5">
-                        <div className="flex flex-col col-span-2 font-extralight">
+
+                        <div className="flex flex-col col-span-5 lg:col-span-2 font-extralight">
                             <span>Session Information</span>
                             <span className="text-xs">Details of the selected session</span>
                         </div>
 
-                        <div className="flex flex-col col-span-3 w-full">
+                        <div className="flex flex-col col-span-5 lg:col-span-3 w-full gap-10">
                             {loading ? (
                                 <div className="grid grid-cols-2 gap-x-5 gap-y-3 animate-pulse">
                                     <div className="bg-gray-200 h-12 rounded"></div>
@@ -71,9 +76,18 @@ export default function ViewSessionPage() {
                                     <div className="lg:col-span-1 col-span-2"><PrimaryInput label="Updated At" value={new Date(sessionDetails?.updatedAt ?? "").toLocaleString() ?? "N/A"} disabled /></div>
                                 </div>
                             )}
+
+                            <div className="h-full flex-row w-full">
+                                <div className="w-full grid grid-cols-2 gap-5">
+                                    <Button className="border-red-600 hover:bg-red-600 border-2 text-red-600 hover:text-white text-sm" >Cancel Session</Button >
+                                    <Button className="bg-primaryGreen text-white hover:bg-secondaryGreen text-sm" onClick={()=>router.replace(`/sessions/video/${sessionDetails?._id}`)}>Start Session</Button >
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
+
             </div>
         </AuthenticatedLayout>
     );
