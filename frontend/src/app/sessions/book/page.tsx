611c02e79c6ec898f65360b71d97e309d3da966e
kkopoku <kk.opoku@outlook.com>
2024-09-18 21:38:09 +0000

    chore: created book session page

---
+++ frontend/src/app/sessions/book/page.tsx
@@ -0,0 +1,9 @@
+import AuthenticatedLayout from "@/components/layouts/AuthenticatedLayout";
+
+export default function BookSessionPage(){
+    return(
+        <AuthenticatedLayout pageName="Book Session">
+            Here is the book sessions
+        </AuthenticatedLayout>
+    )
+}
