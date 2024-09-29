162b54775f1b8458e6d8fe8eb70c1ec046cd794a
kkopoku <kk.opoku@outlook.com>
2024-09-29 04:11:51 +0000

    wip: made vidoe show without audio feedback

--- frontend/src/app/sessions/video/[sessionId]/page.tsx
+++ frontend/src/app/sessions/video/[sessionId]/page.tsx
@@ -4,7 +4,6 @@ import AuthenticatedLayout from "@/components/layouts/AuthenticatedLayout";
 import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client"
 import { useParams } from "next/navigation";
-import { callUser } from "./page.functions";
 
 export default function VideoPage() {
 
@@ -45,11 +44,13 @@ export default function VideoPage() {
         otherUser.current = userID;
       });
 
-      // socketRef.current.on("offer", handleRecieveCall);
+      socketRef.current.on("offer", handleRecieveCall);
 
-      // socketRef.current.on("answer", handleAnswer);
+      socketRef.current.on("answer", handleAnswer);
 
-      // socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
+      socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
+
+      
     })
     .catch((e) => console.log(e));
 
@@ -59,10 +60,130 @@ export default function VideoPage() {
 
   }, []);
 
+  function callUser(userID: string) {
+    console.log("Calling user:", userID);
+    peerRef.current = createPeer(userID);
+    userStream.current?.getTracks().forEach((track) => {
+      console.log("Adding track to peer connection:", track);
+      peerRef.current?.addTrack(track, userStream.current!);
+    });
+  }
+
+  function createPeer(userID: string) {
+    console.log("Creating peer connection for user:", userID);
+    const peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.stunprotocol.org",
+        },
+        {
+          urls: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
+        },
+      ],
+    });
+
+    peer.onicecandidate = handleICECandidateEvent;
+    peer.ontrack = handleTrackEvent;
+    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
+
+    return peer;
+  }
+
+  function handleNegotiationNeededEvent(userID: string) {
+    console.log("Negotiation needed for user:", userID);
+
+    peerRef.current
+      ?.createOffer()
+      .then((offer) => {
+        console.log("Created offer:", offer);
+
+        return peerRef.current?.setLocalDescription(offer);
+      })
+      .then(() => {
+        const payload = {
+          target: userID,
+          caller: socketRef.current?.id,
+          sdp: peerRef.current?.localDescription,
+        };
+        console.log("Sending offer payload:", payload);
+        socketRef.current?.emit("offer", payload);
+      })
+      .catch((e) => console.log(e));
+  }
+
+  function handleRecieveCall(incoming: {
+    sdp: RTCSessionDescriptionInit;
+    caller: string;
+  }) {
+    console.log("Received call from:", incoming.caller);
+
+    peerRef.current = createPeer(incoming.caller);
+    const desc = new RTCSessionDescription(incoming.sdp);
+    peerRef.current
+      ?.setRemoteDescription(desc)
+      .then(() => {
+        userStream.current?.getTracks().forEach((track) => {
+          console.log(
+            "Adding track to peer connection after receiving call:",
+            track
+          );
+          peerRef.current?.addTrack(track, userStream.current!);
+        });
+      })
+      .then(() => {
+        return peerRef.current?.createAnswer();
+      })
+      .then((answer) => {
+        console.log("Created answer:", answer);
+        return peerRef.current?.setLocalDescription(answer);
+      })
+      .then(() => {
+        const payload = {
+          target: incoming.caller,
+          caller: socketRef.current?.id,
+          sdp: peerRef.current?.localDescription,
+        };
+        console.log("Sending answer payload:", payload);
+        socketRef.current?.emit("answer", payload);
+      });
+  }
+
+  function handleAnswer(message: { sdp: RTCSessionDescriptionInit }) {
+    console.log("Received answer:", message.sdp);
+    const desc = new RTCSessionDescription(message.sdp);
+    peerRef.current?.setRemoteDescription(desc).catch((e) => console.log(e));
+  }
+
+  function handleICECandidateEvent(e: RTCPeerConnectionIceEvent) {
+    if (e.candidate) {
+      console.log("Outgoing ICE candidate: ", e.candidate);
+      const payload = {
+        target: otherUser.current,
+        candidate: e.candidate,
+      };
+      socketRef.current?.emit("ice-candidate", payload);
+    }
+  }
+
+  function handleNewICECandidateMsg(incoming: RTCIceCandidateInit) {
+    console.log("Incoming ICE candidate: ", incoming);
+    const candidate = new RTCIceCandidate(incoming);
+    peerRef.current?.addIceCandidate(candidate).catch((e) => console.log(e));
+  }
+
+  function handleTrackEvent(e: RTCTrackEvent) {
+    console.log("Received remote track");
+    if (partnerVideo.current) {
+      partnerVideo.current.srcObject = e.streams[0];
+    }
+  }
+
   return (
     <div className="flex justify-center items-center bg-white h-screen text-black">
-      <div>{sessionId} hi</div>
-      {/* <button className="bg-green-500 rounded-lg p-2 w-44 text-white" onClick={sendTestEvent}>Test</button> */}
+      <video autoPlay muted ref={userVideo} className="w-full lg:w-1/2"></video>
+      <video autoPlay ref={partnerVideo} className="w-full lg:w-1/2"></video>
     </div>
   );
 }
