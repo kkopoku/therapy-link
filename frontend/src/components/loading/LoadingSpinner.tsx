1f52875dd978110a2ec63c3f77fc0fff6ae150b9
kkopoku <kk.opoku@outlook.com>
2024-10-22 22:40:08 +0000

    fix loading spinner behaivoir on prod

--- frontend/src/components/loading/LoadingSpinner.tsx
+++ frontend/src/components/loading/LoadingSpinner.tsx
@@ -4,13 +4,12 @@ interface LoadingSpinnerProps {
     isDark?: string;
     show?: boolean;
     size: number;
-    borderSize: number;
 }
 
-const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({isDark, show = true, size, borderSize}) => {
+const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({isDark, show = true, size}) => {
     return (
         <svg
-            className={`border-[${borderSize}px] border-slate-200 border-b-primaryGreen animate-spin rounded-full max-h-[${size}px] ${!show && `hidden`}`}
+            className={`border-2 border-slate-200 border-b-primaryGreen animate-spin rounded-full max-h-[${size}px] ${!show && `hidden`}`}
             viewBox="0 0 24 24"
         />
     );
