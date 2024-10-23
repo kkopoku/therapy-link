3560a28930139761471db8d7ad954638c1a4b845
kkopoku <kk.opoku@outlook.com>
2024-10-23 21:46:16 +0000

    chore: dynamic loading spinner

--- frontend/src/components/loading/LoadingSpinner.tsx
+++ frontend/src/components/loading/LoadingSpinner.tsx
@@ -3,12 +3,17 @@ import React from "react";
 interface LoadingSpinnerProps {
     isDark?: string;
     show?: boolean;
+    size?: number
+    border?: number
 }
 
-const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({isDark, show = true}) => {
+const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({isDark, show = true, size=20, border=2}) => {
+
+
     return (
         <svg
-            className={`border-2 border-slate-200 border-b-primaryGreen animate-spin rounded-full w-5 ${!show && `hidden`}`}
+            className={`border-slate-200 border-b-primaryGreen animate-spin rounded-full ${!show && `hidden`}`}
+            style={{ width: `${size}px`, height: `${size}px`, borderWidth: `${border}px` }}
             viewBox="0 0 24 24"
         />
     );
