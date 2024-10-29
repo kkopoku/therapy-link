a15dced4d9bcd027db68f4f6073f3502764c3941
kkopoku <kk.opoku@outlook.com>
2024-10-29 22:59:01 +0000

    chore: made navigations responsive

--- frontend/src/components/navigation/BottomNavigation.tsx
+++ frontend/src/components/navigation/BottomNavigation.tsx
@@ -1,102 +1,63 @@
 import Link from "next/link";
 import {
-  BsHouse,
-  BsCalendar,
-  BsChat,
-  BsPerson,
   BsFacebook,
   BsTwitter,
   BsInstagram,
   BsLinkedin,
 } from "react-icons/bs";
+import Logo from "../logo/LogoBlack";
 
 export default function BottomNavigation() {
   return (
-    <nav className="relative bottom-0 w-full bg-[#1A3634] shadow-lg border-t border-black min-h-96 px-10 py-8">
-      <div className="grid grid-cols-6 gap-8 px-20">
+    <nav className="relative bottom-0 w-full bg-[#1A3634] shadow-lg border-t border-black min-h-96 px-5 sm:px-10 lg:px-20 py-8">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
         <div className="col-span-1 flex flex-col gap-y-3">
           <p className="text-xl font-semibold text-slate-300">About Us</p>
           <ul className="text-sm font-light text-slate-100 space-y-1">
-            <li>
-              <Link href="/join-our-network">Join Our Network</Link>
-            </li>
-            <li>
-              <Link href="/careers">Careers</Link>
-            </li>
-            <li>
-              <Link href="/faqs">FAQs</Link>
-            </li>
-            <li>
-              <Link href="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link href="/privacy-policy">Privacy Policy</Link>
-            </li>
-            <li>
-              <Link href="/terms-of-use">Terms Of Use</Link>
-            </li>
-            <li>
-              <Link href="/accessibility">Accessibility</Link>
-            </li>
-            <li>
-              <Link href="/media-kit">Media Kit</Link>
-            </li>
+            <li><Link href="/join-our-network">Join Our Network</Link></li>
+            <li><Link href="/careers">Careers</Link></li>
+            <li><Link href="/faqs">FAQs</Link></li>
+            <li><Link href="/blog">Blog</Link></li>
+            <li><Link href="/privacy-policy">Privacy Policy</Link></li>
+            <li><Link href="/terms-of-use">Terms Of Use</Link></li>
+            <li><Link href="/accessibility">Accessibility</Link></li>
+            <li><Link href="/media-kit">Media Kit</Link></li>
           </ul>
         </div>
 
         <div className="col-span-1 flex flex-col gap-y-3">
           <p className="text-xl font-semibold text-slate-300">Services</p>
           <ul className="text-sm font-light text-slate-100 space-y-1">
-            <li>
-              <Link href="/therapy">Therapy</Link>
-            </li>
-            <li>
-              <Link href="/psychiatry">Psychiatry</Link>
-            </li>
-            <li>
-              <Link href="/counseling">Counseling</Link>
-            </li>
-            <li>
-              <Link href="/support-groups">Support Groups</Link>
-            </li>
-            <li>
-              <Link href="/resources">Resources</Link>
-            </li>
+            <li><Link href="/therapy">Therapy</Link></li>
+            <li><Link href="/psychiatry">Psychiatry</Link></li>
+            <li><Link href="/counseling">Counseling</Link></li>
+            <li><Link href="/support-groups">Support Groups</Link></li>
+            <li><Link href="/resources">Resources</Link></li>
           </ul>
         </div>
 
         <div className="col-span-1 flex flex-col gap-y-3">
           <p className="text-xl font-semibold text-slate-300">Industries</p>
           <ul className="text-sm font-light text-slate-100 space-y-1">
-            <li>
-              <Link href="/contact">Contact Us</Link>
-            </li>
-            <li>
-              <Link href="/support">Support</Link>
-            </li>
-            <li>
-              <Link href="/feedback">Feedback</Link>
-            </li>
+            <li><Link href="/contact">Contact Us</Link></li>
+            <li><Link href="/support">Support</Link></li>
+            <li><Link href="/feedback">Feedback</Link></li>
           </ul>
         </div>
 
         <div className="col-span-1 flex flex-col gap-y-3">
           <p className="text-xl font-semibold text-slate-300">Useful Links</p>
           <ul className="text-sm font-light text-slate-100 space-y-1">
-            <li>
-              <Link href="/contact">Contact Us</Link>
-            </li>
-            <li>
-              <Link href="/support">Support</Link>
-            </li>
-            <li>
-              <Link href="/feedback">Feedback</Link>
-            </li>
+            <li><Link href="/contact">Contact Us</Link></li>
+            <li><Link href="/support">Support</Link></li>
+            <li><Link href="/feedback">Feedback</Link></li>
           </ul>
         </div>
 
         <div className="col-span-2 flex flex-col gap-y-3 items-center">
-          <p className="text-xl font-semibold text-slate-300">Logo</p>
+          <p className="text-xl font-semibold text-slate-300">
+            <Logo theme={"white"}/>
+          </p>
           <p className="text-xl font-semibold text-slate-300">Follow Us</p>
           <div className="flex space-x-4 text-slate-100">
             <Link href="https://www.facebook.com">
--- frontend/src/components/navigation/TopNavigation.tsx
+++ frontend/src/components/navigation/TopNavigation.tsx
@@ -1,8 +1,10 @@
-"use client"
+"use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signOut, useSession } from "next-auth/react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import LogoText from "../logo/LogoText";
 
 interface ButtonItem {
   name: string;
@@ -12,7 +14,12 @@ interface ButtonItem {
 
 const TopNavigation: React.FC = () => {
   const router = useRouter();
-  const { status } = useSession()
+  const { status } = useSession();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
 
   const firstButtonList: ButtonItem[] = [
     { name: "Pricing", style: "", link: "/pricing" },
@@ -23,72 +30,103 @@ const TopNavigation: React.FC = () => {
     { name: "Contact", style: "", link: "" },
   ];
 
-  const secondButtonList: ButtonItem[] = (status === "authenticated") ? [
+  const secondButtonList: ButtonItem[] = status === "authenticated" ? [
     {
       name: "Sign Out",
-      style:
-        "rounded-md py-1 px-2 border border-[#1A3634] bg-slate-200 transition-all duration-500",
+      style: "rounded-md py-1 px-2 border border-[#1A3634] bg-slate-200 transition-all duration-500",
       link: "api/auth/signout",
     },
     {
       name: "Start Assessment",
-      style:
-        "rounded-md py-1 px-2 border text-white border-[#1A3634] bg-[#314845] hover:bg-[#1A3634] transition-all duration-500",
+      style: "rounded-md py-1 px-2 border text-white border-[#1A3634] bg-[#314845] hover:bg-[#1A3634] transition-all duration-500",
       link: "/auth/client-sign-up",
     },
-  ] :
-  [
+  ] : [
     {
       name: "Sign In",
-      style:
-        "rounded-md py-1 px-2 border border-[#1A3634] bg-slate-200 transition-all duration-500",
+      style: "rounded-md py-1 px-2 border border-[#1A3634] bg-slate-200 transition-all duration-500",
       link: "/auth/login",
     },
     {
       name: "Start Assessment",
-      style:
-        "rounded-md py-1 px-2 border text-white border-[#1A3634] bg-[#314845] hover:bg-[#1A3634] transition-all duration-500",
+      style: "rounded-md py-1 px-2 border text-white border-[#1A3634] bg-[#314845] hover:bg-[#1A3634] transition-all duration-500",
       link: "/auth/client-sign-up",
     },
-  ]
+  ];
 
-
-  const firstButtonListStyle =
-    "rounded-md py-1 px-2 bg-slate-100 hover:bg-slate-300 transition-all duration-500";
+  const firstButtonListStyle = "rounded-md py-1 px-2 bg-slate-100 hover:bg-slate-300 transition-all duration-500";
 
   return (
-    <div className="relative top-0 flex flex-row z-s0 w-full items-center justify-between text-sm py-5 bg-white px-10 lg:px-32 shadow-md">
-        <div className="flex flex-row gap-3">
-
-          {firstButtonList.map((item) => (
-            <button
-              key={item.name}
-              className={item.style ? item.style : firstButtonListStyle}
-              onClick={() => router.push(item.link)}
-            >
-              {item.name}
-            </button>
-          ))}
+    <div className="relative top-0 flex flex-row z-50 w-full items-center justify-between text-sm py-5 bg-white px-10 lg:px-32 shadow-md">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex gap-3 w-full lg:w-auto">
+          {/* Hamburger icon for small screens */}
+          <div className="flex flex-row justify-between lg:hidden w-full" onClick={toggleMenu}>
+            <LogoText />
+            {isMenuOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+          </div>
+          {/* Large screen buttons */}
+          <div className="hidden lg:flex flex-row gap-3">
+            {firstButtonList.map((item) => (
+              <button
+                key={item.name}
+                className={item.style ? item.style : firstButtonListStyle}
+                onClick={() => router.push(item.link)}
+              >
+                {item.name}
+              </button>
+            ))}
+          </div>
         </div>
 
-        <div className="flex flex-row gap-3">
+        <div className="hidden lg:flex flex-row gap-3">
           {secondButtonList.map((item) => (
             <button
               key={item.name}
               className={item.style}
-              onClick={() =>{
-                if (item.link !== "api/auth/signout")
-                  router.push(item.link)
-                else
-                  signOut({callbackUrl:"/auth/login"})
+              onClick={() => {
+                if (item.link !== "api/auth/signout") router.push(item.link);
+                else signOut({ callbackUrl: "/auth/login" });
               }}
             >
               {item.name}
             </button>
           ))}
         </div>
+      </div>
 
-
+      {/* Small screen dropdown menu */}
+      {isMenuOpen && (
+        <div className="lg:hidden absolute top-16 left-0 w-full bg-white shadow-md py-5 px-10">
+          <div className="flex flex-col gap-3">
+            {firstButtonList.map((item) => (
+              <button
+                key={item.name}
+                className={item.style ? item.style : firstButtonListStyle}
+                onClick={() => {
+                  setIsMenuOpen(false);
+                  router.push(item.link);
+                }}
+              >
+                {item.name}
+              </button>
+            ))}
+            {secondButtonList.map((item) => (
+              <button
+                key={item.name}
+                className={item.style}
+                onClick={() => {
+                  setIsMenuOpen(false);
+                  if (item.link !== "api/auth/signout") router.push(item.link);
+                  else signOut({ callbackUrl: "/auth/login" });
+                }}
+              >
+                {item.name}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
