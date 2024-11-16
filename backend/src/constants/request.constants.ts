77ee1b9d53f51f8f74d30cb7468e4e728f7e424b
kkopoku <kk.opoku@outlook.com>
2024-11-16 02:07:07 +0000

    chore: created request constants

---
+++ backend/src/constants/request.constants.ts
@@ -0,0 +1,11 @@
+export const CODES = {
+    CREATED: 201,
+    SUCCESS: 200,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500
+}
+
+export default CODES
