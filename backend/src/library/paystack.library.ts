801f8e6a93d263670a0c60c1cb416dad39669b6c
kkopoku <kk.opoku@outlook.com>
2024-11-18 12:53:49 +0000

    chore: added submit otp and checkPendingCharge functions

--- backend/src/library/paystack.library.ts
+++ backend/src/library/paystack.library.ts
@@ -1,5 +1,3 @@
-import { ref } from "joi";
-
 interface DebitProps{
     amount: number;
     email: string;
@@ -7,6 +5,7 @@ interface DebitProps{
     provider: "vod"|"mtn"|"atl"
 }
 export async function debit({amount, email, msisdn, provider}:DebitProps){
+    const logtag = "[paystack.library.ts][debit]"
     const data = {
         amount: amount * 100,
         email,
@@ -16,34 +15,106 @@ export async function debit({amount, email, msisdn, provider}:DebitProps){
           provider,
         },
       };
-    const response = await fetch(`${process.env.PS_URL}/charge`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            Authorization: `Bearer ${process.env.PS_KEY}`,
-        },
-        body: JSON.stringify(data),
-    });
 
-    return await response.json();
+    try{
+        console.log(`${process.env.PS_URL}/charge`)
+        const response = await fetch(`${process.env.PS_URL}/charge`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                Authorization: `Bearer ${process.env.PS_KEY}`,
+            },
+            body: JSON.stringify(data)
+        });    
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`${logtag} Failed with status ${response.status}: ${errorText}`);
+            throw new Error("Failed to charge");
+        }
+
+        return await response.json();
+    }catch(e:any){
+        if(e?.response){
+            console.log(`${logtag} ${JSON.stringify(e.response.data)}`)
+            throw new Error("Something went wrong")
+        }else{
+            console.log(`${logtag} ${e}`)
+            throw new Error("Something went wrong")
+        }
+    }
 }
 
 
 export async function submitOtp(otp:string, reference:string){
+    const logtag = "[paystack.library.ts][submitOtp]"
+
     const data =  {
         otp,
         reference
     }
-    const response = await fetch(`${process.env.PS_URL}/charge/submit_otp`,{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            Authorization: `Bearer ${process.env.PS_KEY}`,
-        },
-        body: JSON.stringify(data)
-    })
 
-    return await response.json()
+    try{
+        const response = await fetch(`${process.env.PS_URL}/charge/submit_otp`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                Authorization: `Bearer ${process.env.PS_KEY}`,
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`${logtag} Failed with status ${response.status}: ${errorText}`);
+            throw new Error("Failed to submit OTP");
+        }
+    
+        return await response.json()
+    }catch(e:any){
+        if(e?.response){
+            console.log(`${logtag} ${JSON.stringify(e.response.data)}`)
+            throw new Error("Something went wrong")
+        }else{
+            console.log(`${logtag} ${e}`)
+            throw new Error("Something went wrong")
+        }
+    }
+}
+
+
+
+export async function checkPendingCharge(reference:string){
+
+    const logtag = "[paystack.library.ts][checkPendingCharge]"
+
+    try{
+        const response = await fetch(`${process.env.PS_URL}/charge/${reference}`,{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                Authorization: `Bearer ${process.env.PS_KEY}`,
+            }
+        })
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`${logtag} Failed with status ${response.status}: ${errorText}`);
+            throw new Error("Failed to submit OTP");
+        }
+    
+        return await response.json()
+    }catch(e:any){
+        if(e?.response){
+            console.log(`${logtag} ${JSON.stringify(e.response.data)}`)
+            throw new Error("Something went wrong")
+        }else{
+            console.log(`${logtag} ${e}`)
+            throw new Error("Something went wrong")
+        }
+    }
+
 }
