9155d9e3409a8756df943eb387196b7b3ac72ca9
kkopoku <kk.opoku@outlook.com>
2024-11-19 21:04:23 +0000

    chore: cleaned index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from 'express'
-import { connectDB } from "./database/connection"
 import userRouter from './routes/user.route'
 import authRouter from './routes/auth.route'
 import clientRouter from './routes/client.route'
 import sessionRouter from "./routes/session.route"
 import dashboardRouter from './routes/dashboard.route'
+import creditRouter from './routes/credit.route'
 import dotenv from "dotenv"
 import cors from "cors"
 import authorize from './middlewares/auth.middleware'
@@ -12,7 +12,6 @@ import authorize from './middlewares/auth.middleware'
 dotenv.config({ path: '../.env' });
 
 const app = express()
-const port = process.env.PORT
 const prefix = "/api/v1"
 
 app.use(express.json());
@@ -29,19 +28,6 @@ app.use(`${prefix}/user`, authorize, userRouter)
 app.use(`${prefix}/session`, authorize, sessionRouter)
 app.use(`${prefix}/client`, authorize, clientRouter)
 app.use(`${prefix}/dashboard`, authorize, dashboardRouter)
-
-// const run = async () => {
-//   try {
-//     await connectDB()
-//     app.listen(port, () => {
-//       console.log(`Server is running on http://localhost:${port}`)
-//     })
-//   } catch (error) {
-//     console.error('Failed to connect to the database', error)
-//   }
-// }
-
-
-// run()
+app.use(`${prefix}/credit`, authorize, creditRouter)
 
 export default app
