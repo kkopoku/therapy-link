600d719a9f4cfe96499c6ce9bc7738e017a95d30
kkopoku <kk.opoku@outlook.com>
2024-11-25 12:08:21 +0000

    chore: added credits

--- backend/src/routes/client.route.ts
+++ backend/src/routes/client.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getClientDetails, getClients } from "../controllers/client.controller"
+import { getClientDetails, getClients, getCredits } from "../controllers/client.controller"
 
 const clientRouter = express.Router();
 
 clientRouter.get("/:id", getClientDetails)
 clientRouter.get("/", getClients)
+clientRouter.get("/credits", getCredits)
 
 export default clientRouter;
