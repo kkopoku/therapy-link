b3270bc2c204878a2086b3b10c5862b67b60bc09
kkopoku <kk.opoku@outlook.com>
2024-12-10 14:27:20 +0000

    chore: created getCreditsPrice and buyCredits function

---
+++ frontend/src/app/plan/page.functions.ts
@@ -0,0 +1,56 @@
+const baseEndpoint = process.env.NEXT_PUBLIC_API_URL
+
+
+export async function getCreditsPrice(token:string, amount:number):Promise<any>{
+    try{
+        const response = await fetch(`${baseEndpoint}/credit/price`, {
+            method: "POST",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                amount
+            })
+        })
+
+        let { data } = await response.json()
+
+        if(!response.ok) {
+            console.log(data)
+            throw new Error ("Something went wrong")
+        }
+
+        return data
+    }catch(e:any){
+        console.log(e)
+        throw new Error ("Something went wrong")
+    }
+}
+
+
+export async function buyCredits(token:string, amount:number):Promise<any>{
+    try{
+        const response = await fetch(`${baseEndpoint}/credit/buy`, {
+            method: "POST",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                number:amount
+            })
+        })
+        let jsonResponse = await response.json()
+        if(!response.ok) {
+            console.log(jsonResponse)
+            throw new Error ("Something went wrong")
+        }
+        return jsonResponse
+    }catch(e:any){
+        console.log(e)
+        throw new Error ("Something went wrong")
+    }
+}
