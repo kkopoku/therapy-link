4c9cdc7c0bbc1c15b6560e782a3f402737a9ec06
kkopoku <kk.opoku@outlook.com>
2024-12-12 09:18:23 +0000

    chore: added checkTransactionStatus route

--- backend/src/routes/credit.route.ts
+++ backend/src/routes/credit.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { buyCredits, submitBuyCreditOtp, getCreditPrice } from "../controllers/transaction.controller"
+import { buyCredits, submitBuyCreditOtp, getCreditPrice, checkTransactionStatus } from "../controllers/transaction.controller"
 
 const creditRouter = express.Router();
 
 creditRouter.post("/buy", buyCredits)
 creditRouter.post("/otp", submitBuyCreditOtp)
 creditRouter.post("/price", getCreditPrice)
+creditRouter.get("/checkTransactionStatus/:id", checkTransactionStatus)
 
 export default creditRouter;
