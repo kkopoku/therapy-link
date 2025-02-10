7e5814464e8ce47ed31d312b3289cbcc72084226
kkopoku <kk.opoku@outlook.com>
2025-02-10 15:38:11 +0000

    chore: multer config

--- backend/src/config/multer.config.ts
+++ backend/src/config/multer.config.ts
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
     fileFilter: (req, file, cb) => {
-        const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp"];
+        const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp","application/pdf"];
         if (!allowedMimeTypes.includes(file.mimetype)) {
             return cb(new Error("Invalid file type. Only images are allowed."));
         }
