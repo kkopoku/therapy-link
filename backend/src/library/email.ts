8c0e9c79f589d2adbb83c40590f79b8977d7ebe2
kkopoku <kk.opoku@outlook.com>
2025-02-10 15:42:03 +0000

    chore: email helper

--- backend/src/library/email.ts
+++ backend/src/library/email.ts
@@ -1,20 +1,27 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
 
 export async function sendEmail(
   recipients: string,
   html: string,
   subject: string
 ) {
+
+  console.log("logging the email:", process.env.EMAIL_USERNAME)
+
   let transporter = nodemailer.createTransport({
-    service: process.env.SERVICE,
+    service: process.env.EMAIL_SERVICE,
     auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: process.env.EMAIL,
+    from: process.env.EMAIL_USERNAME,
     to: recipients,
     subject,
     html,
