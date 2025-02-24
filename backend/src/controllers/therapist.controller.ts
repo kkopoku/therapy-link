ed49344eca4e2453bc3ec13ed66d5bc8d1442c93
kkopoku <kk.opoku@outlook.com>
2025-02-24 03:36:04 +0000

    chore: added logs ang get single therapist

--- backend/src/controllers/therapist.controller.ts
+++ backend/src/controllers/therapist.controller.ts
@@ -3,6 +3,8 @@ import Therapist from "../models/therapist.model"
 import { sendResponse } from "../library/utils.library"
 import Joi from "joi"
 import { objectId, msisdn } from "../library/joi.library"
+import { logger } from "../config/logger.config"
+import Answer from "../models/answer.model"
 
 export async function getTherapists (req: Request, res: Response){
     const tag = "[therapist.controller][getTherapists]"
@@ -14,12 +16,12 @@ export async function getTherapists (req: Request, res: Response){
                 foundTherapists = await Therapist.find({}).lean().exec()
                 break
             default:
-                console.log(`${tag} Unusual user type detected: ${userType}`)
+                logger.info(`${tag} Unusual user type detected: ${userType}`)
                 return sendResponse(res,{message:"Unauthorized", status:"failed"}, 401)
         }
         sendResponse(res,{message:"Therapist Details fetched successfully", status: "success", data: foundTherapists})
     }catch(error:any){
-        console.log(`${tag} Error: ${error}`)
+        logger.info(`${tag} Error: ${error}`)
         return sendResponse(res,{
             message:"Internal Server Error",
             status: "error"
@@ -46,7 +48,7 @@ export async function updateTherapist(req: Request, res: Response){
     delete(value.id)
 
     if (error){
-        console.log(error)
+        logger.info(error)
         return res.status(400).json({
             message: error.details[0].message,
             status: "failed"
@@ -54,7 +56,7 @@ export async function updateTherapist(req: Request, res: Response){
     }
 
     try{
-        console.log(value)
+        logger.info(value)
         const updatedTherapist = await Therapist.findByIdAndUpdate(id, value, {new:true})
         if(!updatedTherapist){
             return sendResponse(res, {
@@ -70,7 +72,53 @@ export async function updateTherapist(req: Request, res: Response){
             message: "test"
         })
     }catch(error:any){
-        console.log(error)
+        logger.info(error)
+        sendResponse(res, {data: null, message: "Internal Server Error", status: "failed"})
+    }
+}
+
+
+export async function getSingleTherapist(req: Request, res: Response){
+    const tag = "[therapist.controller][getSingleTherapist]"
+
+    const schema = Joi.object({
+        id: Joi.string().custom(objectId).required(),
+    })
+
+    const { error, value } = schema.validate(req.params)
+
+    if (error){
+        logger.info(`${tag} ${error}`)
+        return res.status(400).json({
+            message: error.details[0].message,
+            status: "failed"
+        })
+    }
+
+    const { id } = value;
+
+    try{
+        const foundTherapist:any = await Therapist.findById(id).lean().exec()
+        if(!foundTherapist){
+            return sendResponse(res, {
+                data: null,
+                status: "failed",
+                message: "Therapist not found"
+            },404)
+        }
+
+        const answers = await Answer.find({owner: id}).populate({path:"question", select:"question category"})
+        if(answers.length>0){
+            foundTherapist.answers = answers
+        }
+
+        sendResponse(res, {
+            data: foundTherapist,
+            status: "success",
+            message: "Therapist fetched successfully"
+        })
+    }catch(error:any){
+        logger.info(`${tag} Error: ${error}`)
         sendResponse(res, {data: null, message: "Internal Server Error", status: "failed"})
     }
 }
