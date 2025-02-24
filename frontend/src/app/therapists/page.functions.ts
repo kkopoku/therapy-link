4c3f36086da94d1ce34219e42f5b478f06b763a8
kkopoku <kk.opoku@outlook.com>
2025-02-24 03:38:00 +0000

    chore: get single therapist

--- frontend/src/app/therapists/page.functions.ts
+++ frontend/src/app/therapists/page.functions.ts
@@ -1,3 +1,5 @@
+import aws from "aws-sdk"
+
 const baseEndPoint = `${process.env.NEXT_PUBLIC_API_URL}/therapist`
 
 export async function getTherapists(session:any, setTherapists:Function, setLoading:Function){
@@ -25,3 +27,31 @@ export async function getTherapists(session:any, setTherapists:Function, setLoad
         }
     }
 }
+
+
+export async function getSingleTherapist(session:string, therapistId:string, setTherapist:Function, setLoading:Function){
+    if(session){
+        try{
+            setLoading(true)
+            const response = await fetch(`${baseEndPoint}/${therapistId}`, {
+                method: "GET",
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${session}`
+                }
+            })
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+            const { data } = await response.json()
+            setTherapist(data)
+        }catch(e:any){
+            console.log(`Error: ${e}`)
+            throw new Error("Something went wrong")
+        }finally{
+            setLoading(false)
+        }
+    }
+}
+
